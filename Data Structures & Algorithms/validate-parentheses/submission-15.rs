ab81b1fc7daffd90d39e45f1ba8c7decb97c4358
Rehan Ali <rehan.shah70@yahoo.com>
2026-07-08 23:30:42 +0500

    Add: validate-parentheses - submission-15

---
+++ Data Structures & Algorithms/validate-parentheses/submission-15.rs
@@ -0,0 +1,52 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        if s.len() < 2 {
+            return false
+        }
+
+        let mut starting: HashSet<char> = HashSet::new();
+        starting.insert('(');
+        starting.insert('[');
+        starting.insert('{');
+
+        let mut ending: HashSet<char> = HashSet::new();
+        ending.insert(')');
+        ending.insert(']');
+        ending.insert('}');
+
+        let mut pairs: HashMap<char, char> = HashMap::new();
+        pairs.insert(')', '(');
+        pairs.insert(']', '[');
+        pairs.insert('}', '{');
+
+        let mut list_of_starting: Vec<char> = vec![];
+
+        for c in s.chars() {
+            if starting.contains(&c) {
+                list_of_starting.push(c);
+            } else if ending.contains(&c) {
+                if list_of_starting.len() < 1 {
+                    return false;
+                }
+
+                let last_of_starting = list_of_starting[list_of_starting.len() - 1];
+
+
+                if &last_of_starting == pairs.get(&c).unwrap() {
+                    list_of_starting.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        if list_of_starting.len() > 0 {
+            return false;
+        }
+
+        true
+    }
+}

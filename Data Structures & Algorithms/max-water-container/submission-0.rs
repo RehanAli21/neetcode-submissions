fc8e79ef60003426339c2cea3c47371a23cabe9a
Rehan Ali <rehan.shah70@yahoo.com>
2026-07-17 05:52:44 +0500

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.rs
@@ -0,0 +1,31 @@
+impl Solution {
+    pub fn max_area(heights: Vec<i32>) -> i32 {
+        let mut water_volume:i32 = 0;
+
+        let mut left = 0;
+        let mut right = heights.len() - 1;
+
+        while left < right {
+            println!("{left}");
+            println!("{right}");
+            let temp = heights[left].min(heights[right]) * (right as i32 - left as i32);
+
+            if temp > water_volume {
+                water_volume = temp;
+            }
+
+            if heights[left] < heights[right] {
+                left += 1;
+            } else if heights[right] < heights[left] {
+                right -= 1;
+            } else {
+                left += 1;
+                right -= 1;
+            }
+            
+        }
+
+        water_volume
+
+    }
+}

f77e7e91f94c498eb50ec8d14a705f8ec001884c
Rehan Ali <rehan.shah70@yahoo.com>
2026-07-21 05:52:25 +0500

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.rs
@@ -0,0 +1,52 @@
+impl Solution {
+    pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
+        let mut row_index: i32 = -1;
+        let mut l = 0i32;
+        let mut r = matrix.len() as i32 - 1;
+        let last_col_index = matrix[0].len() - 1;
+
+        // finding row in which we have target
+        while l <= r {
+            let mid = l + (r - l) / 2;
+
+            if matrix[mid as usize][last_col_index] == target || matrix[mid as usize][0] == target {
+                return true;
+            } else if matrix[mid as usize][last_col_index] > target && matrix[mid as usize][0] < target {
+                row_index = mid as i32;
+                break;
+            } else if matrix[mid as usize][0] > target {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+
+        }
+
+
+        println!("{}", row_index);
+
+        if row_index == -1 {
+            return false;
+        }
+
+        let arr = &matrix[row_index as usize];
+
+        let mut l = 0i32;
+        let mut r = arr.len() as i32 - 1;
+
+        while l <= r {
+            let mid = l + (r - l) / 2;
+
+            if arr[mid as usize] == target {
+                return true;
+            } else if arr[mid as usize] < target {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        false
+
+    }
+}
